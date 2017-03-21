e85c0be6922b8d42e330421972552404c45a5184
Sheeraz Anwar <Sheeraz Anwar>
2017-03-21 16:51:42 +0500

    api is changed

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
-var express=require('express');
-var parallelfx=require('parallelfx');
-var bodyParser=require('body-parser');
+var express = require('express');
+var bodyParser = require('body-parser');
+var yCurrency = require('y-currency');
 var countries = require('countries-list');
-var port=process.env.PORT||1080;
+var port = process.env.PORT || 3000;
 
+var app = express();
 
-var app=express();
-
-
-
-app.use(bodyParser.urlencoded({
-  extended:true
-}));
-
-
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(bodyParser.json());
 
@@ -21,50 +14,40 @@ app.get('/getCountries',function(req,res){
     var obj = [];
     var keys= Object.keys(countries['countries']);
     keys.forEach(function(key){
-       
-       
          obj.push({
-            name:countries['countries'][key]['name']+[" {"]+countries['countries'][key]['currency']['0']+countries['countries'][key]['currency']['1']+countries['countries'][key]['currency']['2']+["}"],
+            name:countries['countries'][key]['name']+[" ("]+countries['countries'][key]['currency']['0']+countries['countries'][key]['currency']['1']+countries['countries'][key]['currency']['2']+[")"],
             currency:countries['countries'][key]['currency']['0']+countries['countries'][key]['currency']['1']+countries['countries'][key]['currency']['2']
         })
-          
+
     })
-  
+
     res.send(obj);
 });
 
 
-app.post('/',function(req,res){
+app.post('/', function (req, res) {
+
     var reqValue = req.body.value;
     var convertFrom = req.body.from;
     var convertTo = req.body.to;
-    if(reqValue!=null && convertFrom!=null && convertTo!=null){
-   parallelfx.convert({value:parseInt(reqValue), from:convertFrom, to:convertTo}).then(
-    function(resp){
-        var obj = [];
-     
-         obj.push({
-            result:resp.value,
-            rate:resp.rate
-        })
-    
-    res.send(obj);
-         res.status('200').send(resp);
-    },
-    function(err){
-      //  console.log(err)
-        res.status('500').send(err);
-    }
-);
-
+    if (reqValue != null && convertFrom != null && convertTo != null) {
+        yCurrency
+            .convert(parseInt(reqValue), convertFrom, convertTo, function (err, converted) {
+                if (err) {
+                    res
+                        .status('500')
+                        .send(err);
+                } else{
+                    var rate = converted/parseInt(reqValue);
+                res.status('200').send({result: converted , rate: rate});
+                }
+            });
     }else{
-        res.status('400').send({message:'some perameters are missing'});
+        res.status('400').send('please check the perameters, something is missing');
     }
-  //  console.log(req.body);
- 
+
 })
 
-
-app.listen(port,function(){
-  console.log('server is now connected on '+port);
+app.listen(port, function () {
+    console.log('server is now connected on ' + port);
 });
